ad72fde773e57c9831c30a25442ae5e4b6fe53ef
Pavan-max184 <pavannekkanti1234@gmail.com>
2025-04-09 16:58:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 // Configure Socket.io with CORS
 const io = new Server(server, {
   cors: {
-    origin: "https://ephemeral-beijinho-05f29d.netlify.app", // Update this if frontend is deployed elsewhere
+    origin: "team-live-chat.netlify.app", // Update this if frontend is deployed elsewhere
     methods: ["GET", "POST"],
   },
 });
